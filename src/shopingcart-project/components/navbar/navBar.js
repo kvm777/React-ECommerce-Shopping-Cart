72bad73a4e195d9a8802cd57efbfaae4bb7fd2b3
mahesh <koradamahesh2000@example.com>
2024-02-20 17:04:59 +0530

    shopping cart

---
+++ src/shopingcart-project/components/navbar/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./navbar.css"
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        {/* <Link  >Logout</Link> */}
+        <Link className='link' to={"/"} >Shop</Link>
+        <Link className='link' to={"/cart"} >cart</Link>
+        
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/shopingcart-project/shopingCart.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar/navBar';
+import Shop from './pages/shop/shop';
+import Cart from './pages/cart/cart';
+import { ShopcontextProvider } from './context/shop-context';
+// import SignIn from './authentication/signin';
+// import useAuth from './authentication/useAuth';
+
+
+const ShopingCart = () => {
+  return (
+    <ShopcontextProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+            {/* <Route exact path='/' element={ <useAuth />} /> */}
+            <Route path='/shop' element={<useAuth />} />
+
+            {/* <Route exact path='/' element={<SignIn />} /> */}
+            <Route path='/' element={<Shop />} />
+            <Route path='/cart' element={<Cart />} />
+        </Routes>
+    </Router>
+    </ShopcontextProvider>
+  )
+}
+
+export default ShopingCart;
